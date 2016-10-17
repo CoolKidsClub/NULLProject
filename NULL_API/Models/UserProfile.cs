0f32c8af963bb5040314cf867fbbf2c71e24c898
CoolKidsClub <teamcoolkidsclub@gmail.com>
2016-10-17 15:49:35 +0100

    Updated user profile

--- NULL_API/Models/UserProfile.cs
+++ NULL_API/Models/UserProfile.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NULL_API.Models
 {
     public class UserProfile
     {
         public string Name { get; set; }
+        public string Nickname { get; set; }
+        public string Gender { get; set; }
         public string Address { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Occupation { get; set; }
-        public string TwitterHandle { get; set; }
+        public string HomeTown { get; set; }
+        public List<string> Languages { get; set; }
+        public string Religion { get; set; }
+        public string ConnectedFacebookHandle { get; set; }
+        public string ConnectedInstagramHandle { get; set; }
+        public string ConnectedTwitterHandle { get; set; }
     }
 }
